a2526aba2a044f75a384deb01de441b21a0dafcf
Evox <musgoya@gmail.com>
2023-01-21 13:18:35 +0100

    Revert "Update servicehisilicon.cpp"
    This reverts commit 2bef846807d9651b3615c7c31b93466cba91a9b6.

--- servicehisilicon/servicehisilicon.cpp
+++ servicehisilicon/servicehisilicon.cpp
@@ -2084,13 +2084,13 @@ void eServiceHisilicon::setCutList(ePyObject list)
 			continue;
 		}
 		ePyObject ppts = PyTuple_GET_ITEM(tuple, 0), ptype = PyTuple_GET_ITEM(tuple, 1);
-		if (!(PyLong_Check(ppts) && PyLong_Check(ptype)))
+		if (!(PyLong_Check(ppts) && PyInt_Check(ptype)))
 		{
-			eDebug("[eServiceHisilicon] cutlist entries need to be (pts, type)-tuples (%d %d)", PyLong_Check(ppts), PyLong_Check(ptype));
+			eDebug("[eServiceHisilicon] cutlist entries need to be (pts, type)-tuples (%d %d)", PyLong_Check(ppts), PyInt_Check(ptype));
 			continue;
 		}
 		pts_t pts = PyLong_AsLongLong(ppts);
-		int type = PyLong_AsLong(ptype);
+		int type = PyInt_AsLong(ptype);
 		m_cue_entries.insert(cueEntry(pts, type));
 		eDebug("[eServiceHisilicon] adding %08llx, %d", pts, type);
 	}
